1dc4e0c0ba3238eeef2fbc4e1737edc6258c4c8a
CarlGarcia97 <carlosvigar97@gmail.com>
2025-01-17 16:39:47 +0100

    s

--- GreenSnap/app/src/main/java/com/example/greensnap/model/Planta.kt
+++
@@ -1,5 +0,0 @@
-package com.example.greensnap.model
-
-import java.io.Serializable
-
-data class Planta(var id:Int, var nombre_planta:String, var tipo:String, var imagen:String, var id_categoria:Int):Serializable {}
