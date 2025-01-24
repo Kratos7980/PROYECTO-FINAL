dc174041edf32ad3629525979e9f1ec79233e3ea
Kratos7980 <cgarciav17@educastillalamancha.es>
2025-01-24 11:29:44 +0100

    zd

--- GreenSnap/app/src/main/java/com/example/greensnap/adapter/JardinAdapter.kt
+++ GreenSnap/app/src/main/java/com/example/greensnap/adapter/JardinAdapter.kt
@@ -6,11 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.greensnap.view.PantallaPlantas
 import com.example.greensnap.R
 import com.example.greensnap.model.Planta
+import com.example.greensnap.view.PantallaCuidados
 
-class PlantasAdapter (private val listPlantas:ArrayList<Planta>, private val context: Context) : RecyclerView.Adapter<PlantasViewHolder>(){
+class JardinAdapter(private val listPlantas:ArrayList<Planta>, private val context: Context) : RecyclerView.Adapter<PlantasViewHolder>() {
 
     //Creo el viewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantasViewHolder {
@@ -20,10 +20,12 @@ class PlantasAdapter (private val listPlantas:ArrayList<Planta>, private val con
         //
         return PlantasViewHolder(layoutInflater.inflate(R.layout.item_plantas, parent, false))
     }
+
     //Recupero el tamaño de la lista
     override fun getItemCount(): Int {
         return listPlantas.size
     }
+
     //Pongo los datos en el viewHolder
     override fun onBindViewHolder(holder: PlantasViewHolder, position: Int) {
         // Recupero la planta
@@ -32,15 +34,23 @@ class PlantasAdapter (private val listPlantas:ArrayList<Planta>, private val con
         // Pongo los datos en el viewHolder
         holder.nombrePlantaItem.setText(planta.nombre_planta)
         holder.descripcionItem.setText(planta.tipo)
-        val imageResourceId = holder.itemView.context.resources.getIdentifier(planta.imagen,"drawable", holder.itemView.context.packageName)
+        val imageResourceId = holder.itemView.context.resources.getIdentifier(
+            planta.imagen,
+            "drawable",
+            holder.itemView.context.packageName
+        )
         holder.imageItem.setImageResource(imageResourceId)
 
         // Defino en onClick de los items
-        holder.itemView.setOnClickListener{
-            val intent:Intent = Intent(context, PantallaPlantas::class.java)
-            val bundle:Bundle = Bundle()
+        holder.itemView.setOnClickListener {
+            val intent: Intent = Intent(context, PantallaCuidados::class.java)
+            val bundle: Bundle = Bundle()
+            bundle.putSerializable("planta", planta);
             context.startActivity(intent)
         }
-
+        holder.itemView.setOnLongClickListener{
+            println("hola")
+            true
+        }
     }
 }
---
+++ GreenSnap/app/src/main/java/com/example/greensnap/adapter/JardinViewHolder.kt
@@ -0,0 +1,18 @@
+package com.example.greensnap.adapter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.greensnap.databinding.ItemJardinBinding
+
+class JardinViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    //Creo un binding de ItemPlantasBinding
+    val binding = ItemJardinBinding.bind(itemView)
+
+    //Recupero los elementos del xml con el binding
+    val imageItem: ImageView = binding.imageItem
+    val nombrePlantaItem: TextView = binding.nombrePlantaItem
+    val descripcionItem: TextView = binding.descripcionItem
+}
